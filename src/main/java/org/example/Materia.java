e9275106234dc348ddbb3bdde66e1d45909c5f61
Guido <gbassi@frba.utn.edu.ar>
2025-03-31 17:39:51 -0300

    agrego metodos

--- src/main/java/org/example/Materia.java
+++ src/main/java/org/example/Materia.java
@@ -12,6 +12,6 @@ public class Materia {
     }
 
     public boolean cumpleCorrelativas(Set<Materia> materiasAprobadas) {
-        return materiasAprobadas.containsAll(correlativas);
+        return materiasAprobadas.containsAll(correlativa);
     }
 }
